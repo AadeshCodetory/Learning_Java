1a40aa449edb01c70b4cc53a52829c1682c362bd
AadeshCodetory <adeshtiwari869@gmail.com>
2024-06-02 16:38:38 +0530

    first commit

---
+++ Pattern.java
@@ -0,0 +1,45 @@
+
+
+
+public class Pattern {
+    public static void main(String[] args) {
+
+//Qestion 1: Solid Rectangle 
+
+        // this is nested loop concept because external loop contains another loop
+        // outer loop
+        // for (int i=1; i<=4; i++){
+        // //inner loop
+        // for (int j=1;j<=5;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+ //Question 2: print the hollow pattern of Star(*)
+
+        // int m=sc.nextInt();
+        // for(int i=0;i<=n-1;i++)
+        // {
+        // for(int j=0;j<=m-1;j++) {
+        // if(i==0 || j==0 || i==n-1 || j==m-1){
+        // System.out.print("*");
+        // }else{
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+// Question 3: print inverted Half Pyramid pattern of Star(*) 
+
+        // int n = 4;
+        // for (int i = n; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+    }
+}
