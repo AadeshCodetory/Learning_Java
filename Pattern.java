3d8043b19bf51c88d3ebd30538954e92814210a5
AadeshCodetory <adeshtiwari869@gmail.com>
2024-06-13 17:16:57 +0530

    add some advance pattern

--- Pattern.java
+++ Pattern.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Pattern {
     public static void main(String[] args) {
 
-        // Question 1: Solid Rectangle:-
+        // //Question 1: Solid Rectangle:-
 
-        // this is nested loop concept because external loop contains another loop
-        // outer loop
+        // //this is nested loop concept because external loop contains another loop
+        // //outer loop
         // for (int i=1; i<=4; i++){
         // //inner loop
         // for (int j=1;j<=5;j++){
@@ -15,7 +15,7 @@ public class Pattern {
         // System.out.println();
         // }
 
-        // Question 2: print the hollow pattern of Star(*) {ERROR FOND}
+        // // Question 2: print the hollow pattern of Star(*) {ERROR FOND}
 
         // int n=4;
         // int m=5;
@@ -30,7 +30,7 @@ public class Pattern {
         // }
         // }
 
-        // Question 3: print Half Pyramid pattern of Star(*):-
+        //  //Question 3: print Half Pyramid pattern of Star(*):-
         // int n = 4;
         // for (int i = 1; i <= n; i++) {
         // for (int j = 1; j <= i; j++) {
@@ -104,5 +104,112 @@ public class Pattern {
         // }
         // System.out.println();
         // }
+
+        // Question. 10: Butterfly Pattern:-
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+
+        // // upper part
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // // spaces
+        // for (int j = 1; j <= 2 * (n - i); j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+
+        // }
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // // spaces
+        // for (int j = 1; j <= 2 * (n - i); j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+
+        // }
+
+        // Question. 11: Solid Rhombus Pattern:-
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        // for(int j=1;j<=n-i; j++){
+        // System.out.print(" ");
+        // }
+        // for(int j=1; j<=n; j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Question. 12:number pyramid Pattern:-
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Question. 13: Palindromic Pattern:-
+        // Scanner sc= new Scanner(System.in);
+        // int n=sc.nextInt();
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i; j>=1;j--){
+        //         System.out.print(j);
+             
+        //     }
+        //     for(int j=2; j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+    
+        // }
+
+
+        // Question. 14: Diamond Pattern:-
+
+        // int n=4;
+        // //upper half:
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=2*i-1;j++){
+        //         System.out.print("*");
+        //     }
+           
+        //     System.out.println();
+            
+        // }
+        //lower half
+        // for(int i=n;i>=1;i--){
+        //      //lower Pattern:
+        //      for(int j=n-i;j>=1; j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=2*i-1;j>=1;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+       
+    
     }
 }
